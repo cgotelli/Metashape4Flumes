f877a838c8ffe1d21af799dacdff218dbae229ea
cgotelli <96237432+cgotelli@users.noreply.github.com>
2022-02-04 00:28:49 +0100

    Update BulkProcessSetup.m

--- BulkProcessSetup.m
+++ BulkProcessSetup.m
@@ -1,31 +1,55 @@
 
 imageFormat ='.jpg';
 filesPath = uigetdir('C:\Users\cleme\Desktop\photos', 'Path where matfiles are stored');
-filenames = dir(fullfile(filesPath, '*.jpg')); % Gets all the files with *.mat extension inside the folder, and stores the information in a struct-type variable
+content = dir(filesPath);
+dfolders = content([content(:).isdir]);
+dfolders = dfolders(~ismember({dfolders(:).name},{'.','..'}));
+
 
 %%
+for fid = 1 :length(dfolders)
 
-for id = 1:length(filenames)
+    disp(strcat("Working on folder: ", dfolders(fid).name))
 
-    f = filenames(id).name;
+    cd(fullfile(dfolders(fid).folder, dfolders(fid).name))
     
-    disp(f)
+    filenames = dir(fullfile('*.jpg')); % Gets all the files with *.mat extension inside the folder, and stores the information in a struct-type variable
+    
+    for id = 1:length(filenames)
 
-    if strcmp(f(1:3), 'LFT')
-        if ~strcmp(f, strcat('LFT_', sprintf('%03d.jpg', id)))
-            movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('LFT_', sprintf('%03d.jpg', id))));
-        else
-            disp('Did not modify the name')
-        end
-    elseif strcmp(f(1:3), 'RGT')
-        if ~strcmp(f, strcat('RGT_', sprintf('%03d.jpg', id)))
-            movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('RGT_', sprintf('%03d.jpg', id))));
-        else
-            disp('Did not modify the name')
-        end
-    else 
+        f = filenames(id).name;
 
-        disp('Did not modify the name')
+        disp(f)
+
+        if strcmp(f(1:3), 'LFT')
+
+            if ~strcmp(f, strcat('LFT_', sprintf('%03d.jpg', id)))
+
+                movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('LFT_', sprintf('%03d.jpg', id))));
+
+            else
+
+                disp('Did not modify the name')
+
+            end
+
+        elseif strcmp(f(1:3), 'RGT')
+
+            if ~strcmp(f, strcat('RGT_', sprintf('%03d.jpg', id)))
+
+                movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('RGT_', sprintf('%03d.jpg', id))));
+
+            else
+
+                disp('Did not modify the name')
+
+            end
+            
+        else
+
+            disp('Did not modify the name')
+
+        end
 
     end
 
