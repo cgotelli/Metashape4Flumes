889dc569afc4139b472f176352f639b595242a48
cgotelli <96237432+cgotelli@users.noreply.github.com>
2022-02-04 00:09:29 +0100

    BulkProcess update

--- BulkProcessSetup.m
+++ BulkProcessSetup.m
@@ -1,4 +1,32 @@
 
 imageFormat ='.jpg';
-filesPath = uigetdir('C:\Users\EPFL-LHE\Documents\GitHub\Metashape-BulkProcess\photos\100MEDIA', 'Path where matfiles are stored');
+filesPath = uigetdir('C:\Users\cleme\Desktop\photos', 'Path where matfiles are stored');
 filenames = dir(fullfile(filesPath, '*.jpg')); % Gets all the files with *.mat extension inside the folder, and stores the information in a struct-type variable
+
+%%
+
+for id = 1:length(filenames)
+
+    f = filenames(id).name;
+    
+    disp(f)
+
+    if strcmp(f(1:3), 'LFT')
+        if ~strcmp(f, strcat('LFT_', sprintf('%03d.jpg', id)))
+            movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('LFT_', sprintf('%03d.jpg', id))));
+        else
+            disp('Did not modify the name')
+        end
+    elseif strcmp(f(1:3), 'RGT')
+        if ~strcmp(f, strcat('RGT_', sprintf('%03d.jpg', id)))
+            movefile(fullfile(filenames(id).folder, f), fullfile(filenames(id).folder, strcat('RGT_', sprintf('%03d.jpg', id))));
+        else
+            disp('Did not modify the name')
+        end
+    else 
+
+        disp('Did not modify the name')
+
+    end
+
+end
